7cc27543e19e33fdea151ae1ec4c5f88506edc4b
Vladimir Pal (BeDoIt) <mail.vpal@gmail.com>
2015-11-26 11:28:15 +0300

    remove unused router channel

--- src/application/application.js
+++ src/application/application.js
@@ -1,11 +1,7 @@
 import $ from 'jquery';
-import _ from 'lodash';
-import Radio from 'backbone.radio';
 import nprogress from 'nprogress';
 import {Application} from 'backbone.marionette';
 import LayoutView from './layout-view';
-
-let routerChannel = Radio.channel('router');
 
 nprogress.configure({
   showSpinner: false
@@ -16,32 +12,5 @@ export default Application.extend({
     this.$body = $(document.body);
     this.layout = new LayoutView();
     this.layout.render();
-
-    this.listenTo(routerChannel, {
-      'before:enter:route' : this.onBeforeEnterRoute,
-      'enter:route'        : this.onEnterRoute,
-      'error:route'        : this.onErrorRoute
-    });
-  },
-
-  onBeforeEnterRoute() {
-    this.transitioning = true;
-    // Don't show for synchronous route changes
-    _.defer(() => {
-      if (this.transitioning) {
-        nprogress.start();
-      }
-    });
-  },
-
-  onEnterRoute() {
-    this.transitioning = false;
-    this.$body.scrollTop(0);
-    nprogress.done();
-  },
-
-  onErrorRoute() {
-    this.transitioning = false;
-    nprogress.done(true);
   }
 });
